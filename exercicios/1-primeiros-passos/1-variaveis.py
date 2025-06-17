5796023c03fb354e7c6af265918e539efd739c8e
Ricardo Naka <73631724+ricardonaka@users.noreply.github.com>
2025-06-17 16:03:26 +0000

    variaveis test

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,16 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+Empresa = 'Ball'
 # Declare variáveis de cada tipo numérico
-
+Inteiro = '4'
+Fracao = '8.82'
 
 # Declare uma variável do tipo booleano
+Verdade = 'true'
+Mentira = 'false'
+Empresa
+Inteiro
+Fracao
+Verdade
+Mentira
