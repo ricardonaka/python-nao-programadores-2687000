5515e3c672563c846fdf443ed406c23ffe2ac181
Ricardo Naka <73631724+ricardonaka@users.noreply.github.com>
2025-06-18 13:50:10 +0000

    resumo

--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -1,5 +1,15 @@
 # Nesse exercício coletaremos dados de uma estudante, armazenaremos em um dicionário e imprimiremos na tela esse dados em um formato amigável.
 
 # 1. Solicite a estudante os seguintes dados: nome, ano que conheceu o LinkedIn, ano atual e os cursos realizados no LinkedIn Learning separados por virgula em ordem cronológica
+nome = input('nome ')
+anolk = input('ano que conheceu o LinkedIn ')
+anohoje = input('ano atual ')
+cursos = input('cursos separados por virgula ')
+est01 ={'nome':nome,'anolk':anolk,'anohoje':anohoje,'cursos':cursos}
+print(est01.keys())
+print(est01.values())
+print(est01['cursos'])
 # 2. Armazene esses dados em um dicionário
+
+
 # 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcurridos, total de cursos realizados e (apenas) o primeiro e último curso.
