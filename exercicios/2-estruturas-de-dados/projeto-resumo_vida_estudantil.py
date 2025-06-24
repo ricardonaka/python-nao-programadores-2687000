a98e973b1715b874680712a5573533312b8ea676
Ricardo Naka <73631724+ricardonaka@users.noreply.github.com>
2025-06-24 15:51:28 +0000

    fd

--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -5,11 +5,11 @@ nome = input('nome ')
 anolk = input('ano que conheceu o LinkedIn ')
 anohoje = input('ano atual ')
 cursos = input('cursos separados por virgula ')
+
+# 2. Armazene esses dados em um dicionário
 est01 ={'nome':nome,'anolk':anolk,'anohoje':anohoje,'cursos':cursos}
 print(est01.keys())
 print(est01.values())
 print(est01['cursos'])
-# 2. Armazene esses dados em um dicionário
-
 
 # 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcurridos, total de cursos realizados e (apenas) o primeiro e último curso.
