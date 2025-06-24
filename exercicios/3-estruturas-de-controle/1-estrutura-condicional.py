cb6e402fb0702ecd7b052602e0e69cda1ad26b1b
Ricardo Naka <73631724+ricardonaka@users.noreply.github.com>
2025-06-24 17:11:11 +0000

    if else

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,7 +1,15 @@
 # Declare 4 variáveis do tipo numérica
+x = 1
+y = 2
+w = 7
+z = 4
 
 
 # Crie uma estrutura condicional para comparar dois números
+if x > y:
+    print(f' x > y')
+else:
+    print(f' x < y')
 
 
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
@@ -11,9 +19,17 @@
 
 
 # Insira outras condições na estrutura condicional usando o elif
-
-
+if x > y:
+    print(f'x > y')
+elif w > z:
+    print(f'w > z')
+else:
+    print(f'nada verdadeiro')
 # Incremente a estrutura condicional já existente com expressões lógicas utilizando "and" ou "or"
-
-
+if (x > y) and (w > z):
+    print(f'os dois verdadeiros')
+elif (x > y) or (w > z):
+    print(f'um dos dois verdadeiros')
+else:
+    print(f'nenhum verdadeiro')
 # Crie uma estrutura condicional onde mais de uma condição seja verdadeira, e use apenas a palavra reservada "if"
