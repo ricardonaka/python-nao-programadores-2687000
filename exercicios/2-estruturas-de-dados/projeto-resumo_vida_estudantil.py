fad16971499ff2331f3f2531c26582d273279fd0
Ricardo Naka <73631724+ricardonaka@users.noreply.github.com>
2025-06-24 16:35:07 +0000

    vida

--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -1,15 +1,21 @@
 # Nesse exercício coletaremos dados de uma estudante, armazenaremos em um dicionário e imprimiremos na tela esse dados em um formato amigável.
 
 # 1. Solicite a estudante os seguintes dados: nome, ano que conheceu o LinkedIn, ano atual e os cursos realizados no LinkedIn Learning separados por virgula em ordem cronológica
-nome = input('nome ')
-anolk = input('ano que conheceu o LinkedIn ')
-anohoje = input('ano atual ')
+estudante = {}
+estudante['nome'] = input('nome ')
+estudante['anolk'] = int(input('ano que conheceu o LinkedIn '))
+estudante['anohoje'] = int(input('ano atual '))
 cursos = input('cursos separados por virgula ')
-
+estudante['cursos']= cursos.split(', ')
 # 2. Armazene esses dados em um dicionário
-est01 ={'nome':nome,'anolk':anolk,'anohoje':anohoje,'cursos':cursos}
-print(est01.keys())
-print(est01.values())
-print(est01['cursos'])
+
+print(estudante)
+
+
 
 # 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcurridos, total de cursos realizados e (apenas) o primeiro e último curso.
+total_anos = estudante['anohoje'] - estudante['anolk']
+total_cursos = len(estudante['cursos'])
+primeiro_curso = estudante['cursos'][0]
+ultimo_curso = estudante['cursos'][-1]
+print(f'O estudante {estudante["nome"]} esta no linkedin a {total_anos} anos. O primeiro curso foi o {primeiro_curso} e o ultimo curso {ultimo_curso} de um total de {total_cursos} cursos')
